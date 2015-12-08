9f5bf7899ea98ed526e534e2e1e36c7f61b6ad41
Liam Allen <liam89a@gmail.com>
2015-12-08 22:16:22 +0000

    Add VTK & QT to makefile. VTK rendering of simple grid

--- src/README.md
+++ src/README.md
@@ -1,13 +1,46 @@
-CMake details:
-  https://cmake.org/Wiki/Eclipse_CDT4_Generator
-  https://www.dealii.org/8.3.0/index.html -> CMake in user projects
+Install cmake (version >= 2.8.8 required):
+  wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
+  tar xf cmake-3.2.2.tar.gz
+  cd cmake-3.2.2
+  ./configure
+  make
+  sudo apt-get install checkinstall
+  sudo checkinstall
 
+Install Qt (from http://www.vtk.org/Wiki/VTK/Configure_and_Build#Qt_4.8..2A):
+  mkdir qt5.5.1-install && cd qt5.5.1-install
+  wget http://download.qt.io/official_releases/qt/5.5/5.5.1/qt-opensource-linux-x64-5.5.1.run
+  chmod +x qt-opensource-linux-x64-5.5.1.run
+  ./qt-opensource-linux-x64-5.5.1.run
+
+Set up VTK (http://www.vtk.org/Wiki/VTK/Configure_and_Build#Download_VTK_Source_code):
+  Install qt & vtk dependencies:
+    sudo apt-get build-deps qt5-default
+    sudo apt-get install build-essential libxt-dev libnetcdf-dev
+  wget http://www.vtk.org/files/release/6.3/VTK-6.3.0.tar.gz
+  tar xzf VTK-6.3.0.tar.gz
+  mkdir VTK-6.3.0-build && cd VTK-6.3.0-build
+  cmake -DVTK_QT_VERSION:STRING=5 \
+-DQT_QMAKE_EXECUTABLE:PATH=/.../qt/qt5.5.1/5.5/gcc_64/bin/qmake \
+-DVTK_Group_Qt:BOOL=ON \
+-DCMAKE_PREFIX_PATH:PATH=/.../qt/qt5.5.1/5.5/gcc_64/lib/cmake/Qt5 \
+-DBUILD_SHARED_LIBS:BOOL=ON ../VTK-6.3.0
+ (note: you need to use the full path to DCMAKE_PREFIX_PATH, and possibly to QT_QMAKE_EXECUTABLE)
+  make -j<# of cores>
+  sudo checkinstall
+
+Install deal.ii:
+  tar -xvf deal.II-X.Y.Z.tar.gz
+  mkdir dealii-build && cd dealii-build
+  cmake -DCMAKE_INSTALL_PREFIX=/path/to/install/dir ../deal.II
+  make install
+  make test
 
 Creating eclipse project:
   mkdir /home/liam/eclipse/workspace/fluid_flow/build
   cd /home/liam/eclipse/workspace/fluid_flow/build
   mkdir /home/liam/eclipse/workspace/fluid_flow/src
-  cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug ../src
+  cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_VERSION=4.5 ../src
 Note: may need to upgrade cmake depending on the version of eclipse/cmake used 
 
 Import project via:
--- src/main.cc
+++ src/main.cc
@@ -5,23 +5,79 @@
 #include <deal.II/grid/manifold_lib.h>
 #include <deal.II/grid/grid_out.h>
 
+#include <vtkXMLUnstructuredGridReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkDataSetMapper.h>
+#include <vtkExtractEdges.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
-void create_grid()
+std::string create_grid()
 {
-	dealii::Triangulation<2> triangulation;
+    dealii::Triangulation<2> triangulation;
 	dealii::GridGenerator::hyper_cube(triangulation);
 	triangulation.refine_global(4);
 
-	std::ofstream out("grid-1.eps");
+	std::stringstream ss;
 	dealii::GridOut grid_out;
-	grid_out.write_eps(triangulation, out);
-	std::cout << "Grid written to grid-1.eps";
+	grid_out.write_vtu(triangulation, ss);
+    std::ofstream out ("grid.xml");
+    grid_out.write_eps(triangulation, out);
+	std::cout << "Grid written to stringstream";
+	return ss.str();
+}
+
+void render(std::string vtu)
+{
+	  //read all the data from the file
+	  vtkSmartPointer<vtkXMLUnstructuredGridReader> reader =
+	    vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
+	  reader->ReadFromInputStringOn();
+	  reader->SetInputString(vtu);
+	  reader->Update();
+
+      vtkSmartPointer<vtkExtractEdges> extractEdges =
+          vtkSmartPointer<vtkExtractEdges>::New();
+      extractEdges->SetInputConnection(reader->GetOutputPort());
+      extractEdges->Update();
+
+	  //Create a mapper and actor
+	  vtkSmartPointer<vtkDataSetMapper> mapper =
+	    vtkSmartPointer<vtkDataSetMapper>::New();
+      mapper->SetInputConnection(extractEdges->GetOutputPort());
+
+	  vtkSmartPointer<vtkActor> actor =
+	    vtkSmartPointer<vtkActor>::New();
+	  actor->SetMapper(mapper);
+
+	  //Create a renderer, render window, and interactor
+	  vtkSmartPointer<vtkRenderer> renderer =
+	    vtkSmartPointer<vtkRenderer>::New();
+	  vtkSmartPointer<vtkRenderWindow> renderWindow =
+	    vtkSmartPointer<vtkRenderWindow>::New();
+      renderWindow->AddRenderer(renderer);
+	  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+	    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+	  //Add the actor to the scene
+	  renderer->AddActor(actor);
+	  renderer->SetBackground(.3, .6, .3); // Background color green
+
+	  //Render and interact
+	  renderWindow->Render();
+	  renderWindowInteractor->Start();
 }
 
 int main()
 {
-	create_grid();
+    std::string ss = create_grid();
+	render(ss);
 }
 
